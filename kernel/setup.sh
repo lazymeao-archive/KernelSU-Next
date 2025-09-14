1ae36e5b14d5a8960b9a4253b6a46ddf22ba23d9
lazymeao-archive <182461362+lazymeao-archive@users.noreply.github.com>
2025-09-14 19:41:13 +0000

    update: setup.sh for automated builds
    Signed-off-by: lazymeao-archive <182461362+lazymeao-archive@users.noreply.github.com>

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -6,9 +6,9 @@ GKI_ROOT=$(pwd)
 display_usage() {
     echo "Usage: $0 [--cleanup | <commit-or-tag>]"
     echo "  --cleanup:              Cleans up previous modifications made by the script."
-    echo "  <commit-or-tag>:        Sets up or updates the KernelSU-Next to specified tag or commit."
+    echo "  <commit-or-tag>:        Sets up or updates the KernelSU-Next-SUSFS to specified tag or commit."
     echo "  -h, --help:             Displays this usage information."
-    echo "  (no args):              Sets up or updates the KernelSU-Next environment to the latest tagged version."
+    echo "  (no args):              Sets up or updates the KernelSU-Next-SUSFS environment to the latest tagged version."
 }
 
 initialize_variables() {
@@ -31,16 +31,16 @@ perform_cleanup() {
     [ -L "$DRIVER_DIR/kernelsu" ] && rm "$DRIVER_DIR/kernelsu" && echo "[-] Symlink removed."
     grep -q "kernelsu" "$DRIVER_MAKEFILE" && sed -i '/kernelsu/d' "$DRIVER_MAKEFILE" && echo "[-] Makefile reverted."
     grep -q "drivers/kernelsu/Kconfig" "$DRIVER_KCONFIG" && sed -i '/drivers\/kernelsu\/Kconfig/d' "$DRIVER_KCONFIG" && echo "[-] Kconfig reverted."
-    if [ -d "$GKI_ROOT/KernelSU-Next" ]; then
-        rm -rf "$GKI_ROOT/KernelSU-Next" && echo "[-] KernelSU-Next directory deleted."
+    if [ -d "$GKI_ROOT/KernelSU-Next-SUSFS" ]; then
+        rm -rf "$GKI_ROOT/KernelSU-Next-SUSFS" && echo "[-] KernelSU-Next-SUSFS directory deleted."
     fi
 }
 
-# Sets up or update KernelSU-Next environment
+# Sets up or update KernelSU-Next-SUSFS environment
 setup_kernelsu() {
-    echo "[+] Setting up KernelSU-Next..."
-    test -d "$GKI_ROOT/KernelSU-Next" || git clone https://github.com/KernelSU-Next/KernelSU-Next && echo "[+] Repository cloned."
-    cd "$GKI_ROOT/KernelSU-Next"
+    echo "[+] Setting up KernelSU-Next-SUSFS..."
+    test -d "$GKI_ROOT/KernelSU-Next-SUSFS" || git clone https://github.com/lazymeao-archive/KernelSU-Next-SUSFS && echo "[+] Repository cloned."
+    cd "$GKI_ROOT/KernelSU-Next-SUSFS"
     git stash && echo "[-] Stashed current changes."
     if [ "$(git status | grep -Po 'v\d+(\.\d+)*' | head -n1)" ]; then
         git checkout next && echo "[-] Switched to next branch."
@@ -52,7 +52,7 @@ setup_kernelsu() {
         git checkout "$1" && echo "[-] Checked out $1." || echo "[-] Checkout default branch"
     fi
     cd "$DRIVER_DIR"
-    ln -sf "$(realpath --relative-to="$DRIVER_DIR" "$GKI_ROOT/KernelSU-Next/kernel")" "kernelsu" && echo "[+] Symlink created."
+    ln -sf "$(realpath --relative-to="$DRIVER_DIR" "$GKI_ROOT/KernelSU-Next-SUSFS/kernel")" "kernelsu" && echo "[+] Symlink created."
 
     # Add entries in Makefile and Kconfig if not already existing
     grep -q "kernelsu" "$DRIVER_MAKEFILE" || printf "\nobj-\$(CONFIG_KSU) += kernelsu/\n" >> "$DRIVER_MAKEFILE" && echo "[+] Modified Makefile."
